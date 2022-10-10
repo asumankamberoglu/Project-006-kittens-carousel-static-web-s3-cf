4f24ac83e968ad8068ef827304edb2311305f6a1
asuman kamberoğlu <asumankamberoglu@gmail.com>
2022-10-10 09:09:15 +0300

    kittens project file added

---
+++ upload-script.sh
@@ -0,0 +1,21 @@
+# S3 CLI
+aws s3 ls
+
+aws s3 cp static-web/cat0.jpg s3://info.mydevopsjourney.link
+
+aws s3 cp static-web/cat1.jpg s3://info.mydevopsjourney.link
+
+aws s3 cp static-web/cat2.jpg s3://info.mydevopsjourney.link
+
+aws s3 cp static-web/cat3.png s3://info.mydevopsjourney.link
+
+aws s3 cp static-web/penguins.png s3://info.mydevopsjourney.link
+
+aws s3 cp static-web/index.html s3://info.mydevopsjourney.link
+
+aws s3 ls s3://info.mydevopsjourney.link
+
+aws s3 rm s3://info.mydevopsjourney.link
+
+aws s3 rb s3://info.mydevopsjourney.link
+
